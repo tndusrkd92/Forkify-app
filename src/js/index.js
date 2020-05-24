fd1de01edc3ec39ce5e14d723b0674805598929b
Suyeon Kang <tndusrkd92@gmail.com>
2020-05-24 16:17:42 +0900

    feat: Print out results of query on UI

--- src/js/index.js
+++ src/js/index.js
@@ -12,21 +12,23 @@ const state = {};
 
 const controlSearch = async () => {
     // 1) get query from view
-    const query = 'pizza' //TODO
+    const query = searchView.getInput();
 
     if (query) {
         // 2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for the results
+        searchView.clearInput();
+        searchView.clearResult();
 
         // 4) Search for recipes
         await state.search.getResults(); 
 
         // 5) Render result on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
-}
+};
 
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
