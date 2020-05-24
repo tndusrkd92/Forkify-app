d80baf6c5eadaf85ad5454332991c61f72e4c691
Suyeon Kang <tndusrkd92@gmail.com>
2020-05-24 17:03:52 +0900

    Limit title of results less than 17 characters

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,31 @@ export const clearResult = () => {
     elements.searchResList.innerHTML = '';
 };
 
+/*  
+// 'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / new title = ['Pasta']  
+acc: 5 / acc + cur.length = 9 / new title = ['Pasta', 'with']  
+acc: 9 / acc + cur.length = 15 / new title = ['Pasta', 'with', 'tomato']  
+acc: 15 / acc + cur.length = 18 / new title = ['Pasta', 'with', 'tomato']  
+acc: 18 / acc + cur.length = 24 / new title = ['Pasta', 'with', 'tomato']  
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+
+    if (title.length > limit) {                                        // Split : split strings and return as an array.
+        title.split(' ').reduce((acc, cur) => {                      
+            if(acc + cur.length <= limit) {
+                newTitle.push(cur)
+            }
+            return acc + cur.length;                                   // reduce method ruturns a new accumulator.
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(' ')}...`;                             // Join: put elements of an array into strings together.
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markUp = 
     `
@@ -19,7 +44,7 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
